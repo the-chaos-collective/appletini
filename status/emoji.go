1c410f5bd6ad1c7b7dc01003be0dc2e80589c18f
Toby Selway <tobyselway@outlook.com>
2024-12-01 02:01:36 +0000

    Emoji logic

--- status/emoji.go
+++ status/emoji.go
@@ -4,5 +4,30 @@ func Emoji(input PRInfo) []string {
 	if input.Mergeable == MergeableState_Mergeable && (input.Review == ReviewState_Approved || input.Review == ReviewState_NoReviewRequired) {
 		return []string{"🟢"}
 	}
-	return []string{"🔴", "🚧"}
+
+	emoji := []string{"🔴"}
+
+	switch input.Mergeable {
+	case MergeableState_Mergeable:
+		break
+	case MergeableState_Conflict:
+		emoji = append(emoji, "🚧")
+	case MergeableState_Unknown:
+		emoji = append(emoji, "❓")
+	}
+
+	switch input.Review {
+	case ReviewState_Approved:
+		break
+	case ReviewState_NoReviewRequired:
+		break
+	case ReviewState_ChangesRequested:
+		emoji = append(emoji, "📝")
+	case ReviewState_RequiresReview:
+		emoji = append(emoji, "👁‍🗨")
+	case ReviewState_Unknown:
+		emoji = append(emoji, "❔")
+	}
+
+	return emoji
 }
