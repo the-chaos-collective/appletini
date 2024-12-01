c406db01f47e67174ccae2c80cf81763087886d2
Toby Selway <tobyselway@outlook.com>
2024-12-01 00:56:27 +0000

    Render status

--- status/emoji.go
+++ status/emoji.go
@@ -1,5 +1,8 @@
 package status
 
-func Emoji(input PRInfo) string {
-	return ":)"
+func Emoji(input PRInfo) []string {
+	if input.Mergeable == MergeableState_Mergeable && (input.Review == ReviewState_Approved || input.Review == ReviewState_NoReviewRequired) {
+		return []string{"🟢"}
+	}
+	return []string{"🔴", "🚧"}
 }
