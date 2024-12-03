26e8a2ac59af64ede602bd52de6b16c7411d4c5a
Toby Selway <tobyselway@outlook.com>
2024-12-03 09:35:48 +0000

    Remove NullConfig

--- config/migration/migrate.go
+++ config/migration/migrate.go
@@ -22,7 +22,7 @@ func loadAsVersion(filename string, version int) (migration_types.Migratable, er
 		return v2.Load(filename)
 	}
 
-	return migration_types.NullConfig{}, fmt.Errorf("no loader for config v%d", version)
+	return nil, fmt.Errorf("no loader for config v%d", version)
 }
 
 func MigrateTo(logger logging.Logger, filename string, targetVersion int, dumpMigrations bool) (migration_types.Migratable, error) {
@@ -31,12 +31,12 @@ func MigrateTo(logger logging.Logger, filename string, targetVersion int, dumpMi
 	for {
 		version, err := readVersion(filename)
 		if err != nil {
-			return migration_types.NullConfig{}, fmt.Errorf("determining current version: %w", err)
+			return nil, fmt.Errorf("determining current version: %w", err)
 		}
 
 		current, err := loadAsVersion(filename, version)
 		if err != nil {
-			return migration_types.NullConfig{}, fmt.Errorf("loading config (as v%d): %w", version, err)
+			return nil, fmt.Errorf("loading config (as v%d): %w", version, err)
 		}
 
 		if version >= targetVersion {
@@ -50,19 +50,19 @@ func MigrateTo(logger logging.Logger, filename string, targetVersion int, dumpMi
 
 		new, err := current.ToNext()
 		if err != nil {
-			return migration_types.NullConfig{}, fmt.Errorf("migrating config: %w", err)
+			return nil, fmt.Errorf("migrating config: %w", err)
 		}
 
 		if dumpMigrations {
 			err = os.Rename(filename, strings.ReplaceAll(filename, ".json", fmt.Sprintf(".v%d.json", version)))
 			if err != nil {
-				return migration_types.NullConfig{}, fmt.Errorf("renaming old config: %w", err)
+				return nil, fmt.Errorf("renaming old config: %w", err)
 			}
 		}
 
 		err = new.Save(filename)
 		if err != nil {
-			return migration_types.NullConfig{}, fmt.Errorf("saving new config: %w", err)
+			return nil, fmt.Errorf("saving new config: %w", err)
 		}
 
 	}
--- config/migration/migration_types/types.go
+++ config/migration/migration_types/types.go
@@ -4,13 +4,3 @@ type Migratable interface {
 	Save(filename string) error
 	ToNext() (Migratable, error)
 }
-
-type NullConfig struct{}
-
-func (NullConfig) Save(_ string) error {
-	return nil
-}
-
-func (NullConfig) ToNext() (Migratable, error) {
-	return NullConfig{}, nil
-}
